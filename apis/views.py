6856ec1e89429abb64be402a527f91cc4157da20
francis <franciskamau749@gmail.com>
2025-01-02 21:21:40 +0300

    Rest API

---
+++ apis/views.py
@@ -0,0 +1,26 @@
+from rest_framework.decorators import api_view
+
+from apis.models import Student
+
+
+# Create your views here.
+@api_view(['GET'])
+def students_list(request):
+    students = Student.objects.all()
+    return None
+
+
+def single_student(request):
+    return None
+
+
+def update_student(request):
+    return None
+
+
+def delete_student(request):
+    return None
+
+
+def create_student(request):
+    return None
