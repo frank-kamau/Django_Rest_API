94a90d5172e3c7fe41a963cf6edec381d13449be
francis <franciskamau749@gmail.com>
2025-01-02 22:05:55 +0300

    Serializers & created DB

--- apis/views.py
+++ apis/views.py
@@ -1,13 +1,18 @@
+from http import HTTPStatus
+
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from apis.models import Student
+from apis.my_serializers import StudentSerializer
 
 
 # Create your views here.
 @api_view(['GET'])
 def students_list(request):
     students = Student.objects.all()
-    return None
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
 
 
 def single_student(request):
@@ -21,6 +26,10 @@ def update_student(request):
 def delete_student(request):
     return None
 
-
+@api_view(['POST'])
 def create_student(request):
-    return None
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=HTTPStatus.CREATED)
+    return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
